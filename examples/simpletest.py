8ba793bdb3c500da7c8ddc78de5634c542292810
Jacob Thomas <baconbattle@gmail.com>
2017-10-09 10:11:45 -0400

    Edited Data Collection Program

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -147,5 +147,9 @@ while True:
     t = time.asctime()
     t = (time.time() * 1000) - start_time
     f.write(str(t) + "," + str(heading) + "," + str(roll) + "," + str(pitch) + "," + str(accel_x) + "," + str(accel_y) + "," + str(accel_z) + "," +  str(sys) + "," + str(gyro) + "," + str(accel) + "," + str(mag) + "\n")
-    data_time - start_time <= 0
+    if(data_time * 1000 - start_time <= 0):
+        break
     #time.sleep(.25)
+print("Data Acquisition Completed.")
+input("Press A key to Exit...")
+exit(0)
